c91f428aed320009297b2a62125fc5f721ec3a8d
Miguel Adolfo Sanchez Galarza <miguelsnchezg@gmail.com>
2021-01-28 00:13:54 -0400

    version 1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,5 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click", saludo);
 
 function saludo() {
-    document.getElementById("saludo").innerHTML = "Bienvenidos a Git and GitHub <br/> Les desea Miguel Sanchez";
+    document.getElementById("saludo").innerHTML = "Una cordial bienvenida a Git and GitHub <br/> les desea Miguel Sanchez";
 }
